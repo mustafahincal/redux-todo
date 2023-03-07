90aa9875f78a822d45e9a123c1897eea5c3a059f
mustafahincal <mustafahncal@gmail.com>
2023-03-07 14:31:12 +0300

    add addTodo function to reducers

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,25 @@
+import { nanoid } from "@reduxjs/toolkit";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/todos/todosSlice";
 const Form = () => {
+  const [title, setTitle] = useState<string>("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    dispatch(addTodo({ id: nanoid(), title, completed: false }));
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         className="new-todo"
         placeholder="What needs to be done?"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         autoFocus
       />
     </form>
